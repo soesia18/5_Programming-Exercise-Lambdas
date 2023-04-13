48dfd7b716937f9d6381801ce79adc359cf46682
simon <simon.schoeggler@hotmail.com>
2023-04-13 09:03:00 +0200

    Initial commit

---
+++ src/main/java/at/kaindorf/lambdas/bl/EventBL.java
@@ -0,0 +1,11 @@
+package at.kaindorf.lambdas.bl;
+/**
+* <h3>Created by IntelliJ IDEA.</h3><br>
+* <b>Project:</b> Programming-Exercise - Lambdas<br>
+* <b>User:</b> Simon SchÃ¶ggler<br>
+* <b>Date:</b> 12. April 2023<br>
+* <b>Time:</b> 21:27<br>
+*/
+
+public class EventBL {
+}
---
+++ src/main/java/at/kaindorf/lambdas/data/Event.java
@@ -0,0 +1,12 @@
+package at.kaindorf.lambdas;
+
+/**
+ * <h3>Created by IntelliJ IDEA.</h3><br>
+ * <b>Project:</b> Programming-Exercise - Lambdas<br>
+ * <b>User:</b> Simon SchÃ¶ggler<br>
+ * <b>Date:</b> 12. April 2023<br>
+ * <b>Time:</b> 21:25<br>
+ */
+
+public class Event {
+}
